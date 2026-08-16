763bbdaf24b215bc10f00c16f2397edbc4bae53e
rashmikaspal29 <rashmikaspal15@gmail.com>
2026-08-15 21:12:47 -0400

    areaOfCircle

---
+++ Review/2/circle/areaOfCircle/src/Circle.java
@@ -0,0 +1,13 @@
+public class Circle {
+
+    public static void main(String[] args) throws Exception {
+        
+        int r = 5;
+        final double PI = 3.14157;
+
+        double area = PI * r * r;
+
+        System.out.println("The area of a circle is "+ area);
+        
+    }
+}
